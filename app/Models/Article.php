4c5e98ae16aa1b1004186e31f27730dc60d970c4
Tommy <tommycontreras34@gmail.com>
2023-08-25 22:18:30 -0400

    Relationships defined in models between user, article, category and tag

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'slug', 'excerpt', 'description', 'status', 'user_id', 'category_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
